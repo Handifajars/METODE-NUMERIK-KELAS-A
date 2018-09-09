024ed0f692970be37b9ff181a9a5204ab7bd1656
nurlisaaulia <43093320+nurlisaaulia@users.noreply.github.com>
2018-09-09 22:32:41 +0700

    Update METNUM(17081010019).cpp
    UPDATE HEHE

--- METNUM(17081010019).cpp
+++ METNUM(17081010019).cpp
@@ -6,12 +6,14 @@
 int main()
 {
     double x, result;
+	printf("Nama : Nurlisa Aulia\n");
+	printf("NPM : 17081010019\n\n");
 	printf("Masukkan x (dalam radian): ");
 	scanf("%lf", &result);
 	
 	x = cos (result);
 	
-	printf("\n hasil dari cos(%.2lf) = %.2lf\n", result, x);
-	
+	printf("Hasil dari cos(%.2lf) = %.2lf\n\n", result, x);
+	printf("THANKYOU!\n");
 	return 0;
 }
