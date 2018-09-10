54c84dd6c2e37246b947b108a3e924acbc8c22e5
Irwan Firmansyah <43115047+irwanf10@users.noreply.github.com>
2018-09-10 10:58:18 +0700

    METNUM(17081010094) (#39)

---
+++ METNUM (17081010094).cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+ 
+int faktorial(int n)
+{
+if (n==0) 
+	return 1;
+else
+	return n*faktorial(n-1);
+}
+ 
+int main()
+{
+	float x,y,z;
+	double b,X0;
+		int i;
+		int hasil=1;
+	
+	printf("Masukan Input = ");
+		scanf("%f",&x);
+		x=x*3.14159/180;
+		
+{
+	hasil=i*hasil;	
+}
+	b=(x-X0);
+	b=b/hasil;
+	b=b*x;
+	b=b*-1;
+	printf("hasil %lf\n",x);	
+ }
